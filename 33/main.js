142f04e743b5a42dde3c1b602cf5d2cf97d5dbc8
Mitlond <comp122012016@mail.ru>
2016-09-15 17:58:49 +0300

    JS

---
+++ 33/main.js
@@ -0,0 +1,108 @@
+/*function res( ) {
+	var one=1;
+	var two=2;
+	var tri=3;
+	sum=one+two+tri;
+	return sum;
+}
+console.log(res());
+
+function raz( ) {
+	var parseInt='текст47';
+	return parseInt;
+}
+console.log(parseInt());
+function zov (obj,objName) {
+	var  result= "4";
+  for (var i in obj) {
+    if (obj.(i)) {
+        result += objName + "." + i + " = " + obj[i] + "\n";
+    }
+  }
+  return result;
+}
+console.log(typeof(objName));
+
+
+	var a = 0
+	var s = 1+2+3
+	if(p=2){
+		
+	}
+	else if(a > 0){
+		
+	}
+	else if(s = 1,2,3){
+		
+	}
+	else{
+		
+	}
+}
+console.log(s);
+
+
+var a = 2+2
+
+	switch (a) {
+
+	  case 3:
+
+	    
+
+	    break
+
+	  case 4:
+	    
+	    break
+
+	  case 5:
+
+	   
+
+	    break
+
+	  default:
+	} 
+	console.log(a); 
+	
+	function res(x){
+	var x=7;
+	var y=3;
+	while(7+3)
+		x+y;
+	do (x+y);
+	while(7+3);
+	for(var x=0;x<78;i++);
+	
+	}
+	console.log(x);
+	
+for(var i=7;i<78;i++){
+	if(i==5){
+		num++;
+		continue
+	}
+console.log(i);
+
+var p=object.SumBellForGoTo;{atlantic:78; num:7.8; str_one:'Zhurin'; logic:B:{0,1}}
+for(var SumBellForGoTo in num);
+console.log(p.atlantic);*/
+var a = new Object();
+  {
+    Lux:55;
+    aqua:'piyavka';
+    tenebris:7.8;
+    B:0.1;
+  
+  function Zerrov (o,p){
+  for(prop in p){
+	  o[prop]=p[prop];
+  }
+  return o;
+  };
+}
+console.log(a);
+
+
+	
